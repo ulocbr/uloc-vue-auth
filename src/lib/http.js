d6d32bbb16c6eb09c4848eaa7278bbf3238642bc
Tiago Felipe <tiago@tiagofelipe.com>
2020-05-09 16:05:33 -0300

    Add cache to http

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import { throttleAdapterEnhancer } from 'axios-extensions'
 
 export const http = (function (options) {
-  return axios.create(Object.assign({}, options))
+  return axios.create(Object.assign({
+    headers: { 'Cache-Control': 'no-cache' },
+    // cache will be enabled by default
+    adapter: throttleAdapterEnhancer(axios.defaults.adapter, 2 * 1000)
+  }, options))
 })()
 
 /* export default {
